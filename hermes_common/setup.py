b4a7d9bb1742dbdb92a152f1d83a61e0689c50f5
vladimit <blackjack@repak2.localdomain>
2011-10-03 17:58:51 +0200

    build for c++

--- hermes_common/setup.py
+++ hermes_common/setup.py
@@ -4,5 +4,5 @@ from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hermes_common", ["hermes_common.pyx"])]
+    ext_modules = [Extension("hermes_common", ["hermes_common.pyx"],language="c++")]
 )
