28d4a3a4a9a13bf5d0ed9b6a99028010e0935f96
Vladimir Cerny <blackvladimir@gmail.com>
2011-11-19 15:18:09 +0100

    space h1 declaration

--- hermes2d/setup.py
+++ hermes2d/setup.py
@@ -55,6 +55,7 @@ setup(
     "src/views/linearizer_base.pxi",
     "include/views/linearizer.pxd",
     "src/views/linearizer.pxi",
+    "include/space/space_h1.pxd",
     "include/mesh/mesh_reader_h2d_xml.pxd",
     "src/mesh/mesh_reader_h2d_xml.pxi"
     ],language="c++",libraries=["hermes2d"])]
