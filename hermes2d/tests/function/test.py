f19853515007310ada2f2cdbd7a2d1f795c1df98
Lukas <lukas.korous@gmail.com>
2011-11-29 20:02:54 +0200

    Test linearizer.

--- hermes2d/tests/function/test.py
+++ hermes2d/tests/function/test.py
@@ -1,5 +1,6 @@
 import hermes_common
 import hermes2d
+import os
 
 mesh=hermes2d.PyMesh()
 reader=hermes2d.PyMeshReaderH2DXML()
@@ -9,6 +10,10 @@ viewer=hermes2d.PyScalarView()
 reader.load("domain.xml",mesh)
 
 sln = hermes2d.PyConstantSolutionReal(mesh, 1.0)
+
+linearizer = hermes2d.PyLinearizer()
+linearizer.save_solution_vtk(sln, "solution.vtk", "solution")
+os.system("paraview --data=solution.vtk")
 
 # Test of ScalarView
 viewer.show(sln)
