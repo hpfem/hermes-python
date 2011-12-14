feab02317e6d21e6270bc04b7435e7ea54294f89
Vladimir Cerny <blackvladimir@gmail.com>
2011-12-14 11:44:41 +0100

    fix calculation test

--- hermes2d/tests/calculation/definitions.py
+++ hermes2d/tests/calculation/definitions.py
@@ -1,5 +1,6 @@
 import hermes2d
 class PyCustomWeakFormPoisson(hermes2d.PyCustomWeakFormReal):
   def __init__(self, mat_al, mat_cu):
+    self.super(1)
     self.add_matrix_form(hermes2d.PyDefaultJacobianDiffusion(0,0, mat_al))
     self.add_matrix_form(hermes2d.PyDefaultJacobianDiffusion(0,0, mat_cu))
