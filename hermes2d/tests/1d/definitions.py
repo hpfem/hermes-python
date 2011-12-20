b4da48b7ec6d0f46d491b3c1e15ee728d293b4fd
Lukas <lukas.korous@gmail.com>
2011-12-20 11:37:43 +0100

    Add 1d test.

---
+++ hermes2d/tests/1d/definitions.py
@@ -0,0 +1,9 @@
+import hermes2d
+class PyCustomWeakFormPoisson(hermes2d.PyCustomWeakFormReal):
+  def __init__(self, mat_al, mat_cu):
+    self.super(1)
+    self.add_matrix_form(hermes2d.PyDefaultJacobianDiffusion(0,0, mat_al))
+    self.add_matrix_form(hermes2d.PyDefaultJacobianDiffusion(0,0, mat_cu))
+    self.add_vector_form(hermes2d.PyDefaultResidualDiffusion(0, mat_al))
+    self.add_vector_form(hermes2d.PyDefaultResidualDiffusion(0, mat_cu))
+    self.add_vector_form(hermes2d.PyDefaultVectorFormVol(0))
